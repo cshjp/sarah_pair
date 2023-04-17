f6aad050e1fb880c55e08d87b54a3a2bf17d7ab2
sarahc-dev <sarahclements@hotmail.co.uk>
2023-04-17 15:01:25 +0100

    add todolist

--- lib/todo_list.rb
+++ lib/todo_list.rb
@@ -5,14 +5,29 @@ class TodoList
 
   def add(todo) # todo is an instance of Todo
     # Returns nothing
+    @todolist << todo
   end
 
   def incomplete
     # Returns all non-done todos
+    incomplete = []
+    @todolist.each do |todo|
+      if !todo.done?
+        incomplete << todo
+      end
+    end
+    return incomplete
   end
 
   def complete
     # Returns all complete todos
+    completed = []
+    @todolist.each do |todo|
+      if todo.done?
+        completed << todo
+      end
+    end
+    return completed
   end
 
   def give_up!
--- spec/todo_list_int_spec.rb
+++ spec/todo_list_int_spec.rb
@@ -1,6 +1,38 @@
 require "todo_list"
+require "todo"
 
 describe TodoList do
   describe '#add' do
     context "when given a todo" do
-      it ""
+      it "adds a Todo to Todolist" do
+        todo = Todo.new("get milk")
+        todolist = TodoList.new()
+        result = todolist.add(todo)
+        expect(result).to eq [todo]
+      end
+    end
+  end
+
+  describe '#incomplete' do
+    it "returns all non-done todos" do
+      todo1 = Todo.new("get milk")
+      todo2 = Todo.new("cook dinner")
+      todolist = TodoList.new()
+      todolist.add(todo1)
+      todolist.add(todo2)
+      expect(todolist.incomplete).to eq [todo1, todo2]
+    end
+  end
+
+  describe '#complete' do
+    it "returns all done todos" do
+      todo1 = Todo.new("get milk")
+      todo2 = Todo.new("cook dinner")
+      todo1.mark_done!
+      todolist = TodoList.new()
+      todolist.add(todo1)
+      todolist.add(todo2)
+      expect(todolist.complete).to eq [todo1]
+    end
+  end
+end
